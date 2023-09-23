ccbfdad3a0086a5fb9666a17cb4a08af38c3a9e3
asifulmamun <asifulmamun@gmail.com>
2023-09-23 18:44:37 +0600

    get response correct url

--- upload.php
+++ upload.php
@@ -23,21 +23,30 @@ else:
     if ($uploaded_image && $uploaded_image['size'] > 0) {
         
         $upload_dir = wp_upload_dir();
+    
+        $_renameFile = date('d-m-Y') . '_' . rand(1, 100) . "_" . str_replace(" ", "_", basename($uploaded_image['name']));
         
-
-        $_renameFile = date('d-m-Y') . '_' . rand(1,100) . "_" . str_replace(" ", "_", basename($uploaded_image['name']));
-
         $target_path = $upload_dir['path'] . '/' . $_renameFile;
         move_uploaded_file($uploaded_image['tmp_name'], $target_path);
-
-        // Additional processing can be done here
-        // For example, you can save the image URL in the order meta or perform other operations
-
-        $response = [
-            'message' => 'Success uploading image ' . $target_path
-        ];
         
+
+
+         // Path Sanitizer
+        $position = strpos($target_path, '/wp-content');
+        if($position !== false) {
+            $image_path = substr($target_path, $position);
+            $image_url = get_site_url() . $image_path;
+        } else {
+            $image_url = '';
+        }
+        
+        // Response Success with image url
+        $response = [
+            'message' => 'Success uploading image ' . $image_url
+        ];
+    
     } else {
+        // If no image found
         $response = [
             'message' => 'Error uploading image.'
         ];
